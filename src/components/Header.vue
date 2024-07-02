8bd3a7fa2bd782f8c4ba71d361aa45a73c8b542b
poker-cats-co <hasiradoo@starowl.dev>
2024-07-02 13:38:09 +0200

    added submenu to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,8 @@
 <template>
-  <header class="max-w-screen-lg mx-auto w-full">
-    <section class="flex items-center justify-between">
+  <header class="relative w-full border-b border-backgroundSecondary shadow-lg">
+    <section class="flex items-center justify-between max-w-screen-xl px-6 mx-auto">
       <!-- Navigation for smaller screens -->
       <!-- Uncomment this section if needed -->
-      
       <Sheet>
         <SheetTrigger as-child class="lg:hidden">
           <Button variant="ghost" size="icon">
@@ -31,43 +30,123 @@
         </SheetContent>
       </Sheet>
 
-      <router-link to="/">
-        <Logo class="mix-blend-difference" size="40" />
+      <router-link to="/" class="mr-4">
+        <Logo size="40" />
         </router-link>
       <!-- Main Navigation Menu -->
-      <NavigationMenu class="max-lg:hidden">
+      <ul class="flex items-center">
+        <button @click="toggleMenu" class="flex items-center">
+          Plugins
+          <OutlineChevronRight size="16" class="ml-2 rotate-90" />
+        </button>
+        <!-- Sub Navigation Menu -->
+
+        <section class="bg-background w-screen absolute left-0 top-[calc(100%+1px)] z-20 shadow-lg" v-if="isMenuOpen">
+          <nav class="mx-auto max-w-screen-xl grid grid-cols-5">
+            <section class="w-full h-full col-span-3 pt-8 pb-9 pr-8 grid grid-cols-2 gap-8">
+              <div v-for="(item, i) in dropdownItems" :key="i">
+                  <h4 class="text-foreground text-sm">Category</h4>
+                  <ul class="grid grid-cols-1 gap-6 mt-5 pl-2">
+                    <li v-for="(subitem, j) in item.subitems" :key="j">
+                      <router-link v-if="!subitem.external" :to="subitem.path" class="block transition-colors text-sm text-foreground/40 hover:text-foreground/60">
+                        <div class="flex gap-4">
+                          <OutlineDiscord class="text-foreground"/>
+                          <div class="flex flex-col">
+                            <h4 class="text-foreground text-lg font-bold">title</h4>
+                            {{ subitem.title }}
+                          </div>
+                        </div>
+                      </router-link>
+                      <a v-else :href="subitem.path" class="block text-sm text-foreground/30 hover:text-foreground/70" target="_blank">{{ subitem.title }}</a>
+                    </li>
+                  </ul>
+                </div>
+              </section>
+              <div class="col-span-2 h-full w-full relative pt-8 pb-9 pl-8 pr-8 z-10">
+                <p class="text-muted-foreground text-sm font-medium">Spotlight</p>
+                <section class="grid grid-cols-1 gap-6 mt-5">
+                  <ul class="flex flex-col space-y-4">
+                    <router-link to="#" class="group">
+                      <article class="flex gap-4">
+                          <img src="../assets/hero.png" class="rounded-xl max-w-48" />
+  
+                        <div class="flex flex-col">
+                          <div class="flex flex-1 flex-col">
+                            <h4 class="text-lg font-bold line-clamp-1 text-ellipsis group-hover:text-white transition-colors duration-200">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+                            <p class="text-sm text-muted-foreground line-clamp-2 text-ellipsis group-hover:text-foreground/80 transition-colors duration-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi et deleniti non exercitationem ex similique inventore impedit est perspiciatis amet, aliquid, explicabo modi ut, illum quae vero quod ullam dolorem!</p>
+                          </div>
+                          <Button as-child variant="link" size="link" class="w-fit">
+                            <router-link to="#">
+                              Read article
+                              <OutlineChevronRight size="16" class="ml-1 group-hover:translate-x-1 transition-all" />
+                            </router-link>
+                          </Button>
+                        </div>
+                      </article>
+                    </router-link>
+                    <router-link to="#">
+                      <article class="flex gap-4 group">
+                          <img src="../assets/hero.png" class="rounded-xl max-w-48" />
+                        <div class="flex flex-col">
+                          <div class="flex flex-1 flex-col">
+                            <h4 class="text-lg font-bold line-clamp-1 text-ellipsis group-hover:text-white transition-colors duration-200">title</h4>
+                            <p class="text-sm text-muted-foreground line-clamp-3 text-ellipsis group-hover:text-foreground/80 transition-colors duration-200">description</p>
+                          </div>
+                          <Button as-child variant="link" size="link" class="w-fit">
+                            <router-link to="#">
+                              Read article
+                              <OutlineChevronRight size="16" class="ml-1 group-hover:translate-x-1 transition-all" />
+                            </router-link>
+                          </Button>
+                        </div>
+                      </article>
+                    </router-link>
+                  </ul>
+                </section>
+                <div class="absolute top-0 left-0 right-0 w-screen h-full bg-card-hover -z-10" />
+              </div>
+          </nav>
+        </section>
+      </ul>
+      <!-- <NavigationMenu class="max-lg:hidden">
         <NavigationMenuList>
           <NavigationMenuItem v-for="(item, i) in items" :key="i">
-            <router-link v-if="!item.external" :to="item.path" :class="cn(navigationMenuTriggerStyle(), 'rounded-full')">{{ item.title }}</router-link>
-            <NavigationMenuLink v-else :href="item.path" :class="cn(navigationMenuTriggerStyle(), 'rounded-full')" target="_blank">{{ item.title }}</NavigationMenuLink>
+            <router-link v-if="!item.external" :to="item.path" :class="cn(navigationMenuTriggerStyle(), 'rounded-full text-muted-foreground')">{{ item.title }}</router-link>
+            <NavigationMenuLink v-else :href="item.path" :class="cn(navigationMenuTriggerStyle(), 'rounded-full text-muted-foreground')" target="_blank">{{ item.title }}</NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
-      </NavigationMenu>
+      </NavigationMenu> -->
 
       <!-- User Navigation -->
       <nav class="flex items-center space-x-3 ml-auto">
+        <Button variant="premium" as-child  size="defaultIcon">
+          <router-link to="#">
+            <OutlineCrown class="mr-2"/>
+            Premium
+          </router-link>
+        </Button>
         <Button as-child size="defaultIcon" v-if="!user">
           <a href="https://api.rabbittale.co/login">
             <SolidDiscord class="mr-2" />
             Login with Discord
           </a>
         </Button>
-
-        <Button as-child v-if="user" class="rounded-full">
-          <router-link to="/account"> Manage Servers </router-link>
-        </Button>
-
         <Popover v-if="user">
-          <PopoverTrigger class="h-10">
-            <Avatar>
+          <PopoverTrigger class="h-10 flex items-center gap-2">
+            <Avatar class="mr-2">
               <AvatarImage :src="user.avatarUrl" :alt="user.username" />
               <AvatarFallback>{{ avatarFallbackText }}</AvatarFallback>
             </Avatar>
+            {{ user.globalName }}
+            <OutlineChevronRight size="16" class="rotate-90 transition-transform" />
           </PopoverTrigger>
-          <PopoverContent align="end" class="w-52 p-2 rounded-xl space-y-2">
-            <Button as-child size="default" variant="ghost" class="w-full justify-start rounded-md">
-              <router-link to="/account"> Account </router-link>
-            </Button>
+          <PopoverContent sideOffset="1" align="end" class="w-52 p-2 rounded-xl space-y-2">
+            <section>
+              <h4 class="ml-3 text-xs text-foreground font-bold uppercase py-3">Tiny Rabbit</h4>
+              <Button as-child size="default" variant="ghost" class="w-full justify-start">
+                <router-link to="/dashboard">My Servers</router-link>
+              </Button>
+            </section>
             <Separator />
             <Button as-child size="default" variant="ghost" @click="logout" class="w-full justify-start rounded-md">
               <router-link to="/"> Logout </router-link>
@@ -75,11 +154,11 @@
           </PopoverContent>
         </Popover>
 
-        <Button variant="ghost" size="icon" @click="toggleTheme" class="rounded-full">
+        <!-- <Button variant="ghost" size="icon" @click="toggleTheme" class="rounded-full">
           <OutlineClearNight :class="['absolute rotate-0 scale-100 transition-all', { 'dark:-rotate-90 dark:scale-0': isDark }]" />
           <OutlineSunny :class="['absolute rotate-90 scale-0 transition-all', { 'dark:rotate-0 dark:scale-100': isDark }]" />
           <span class="sr-only">Toggle theme</span>
-        </Button>
+        </Button> -->
       </nav>
     </section>
   </header>
@@ -94,6 +173,11 @@ import {
 	OutlineClearNight,
 	SolidDiscord,
 	OutlineHome,
+  OutlineChevronRight,
+  OutlineDiscord,
+  SolidCarrot,
+  OutlineCarrot,
+  OutlineCrown,
 } from '@/components/ui/icons'
 import { Button } from '@/components/ui/button'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
@@ -123,11 +207,14 @@ import { useAuth } from '@/utils/auth'
 import { useRouter } from 'vue-router'
 import { cn } from '@/lib/utils'
 import { Logo } from '@/components/ui/icons'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem } from './ui/dropdown-menu'
+import { AspectRatio } from './ui/aspect-ratio'
+
 
 const items = [
 	{
 		path: '/bot',
-		title: 'Tiny Rabbit Bot',
+		title: 'Tiny Rabbit',
 		iconSolid: SolidHome,
 		iconOutline: OutlineHome,
 	},
@@ -158,6 +245,26 @@ const items = [
 	},
 ]
 
+const dropdownItems = [
+  {
+    title: 'Moderation & Server Management',
+    subitems: [
+      { path: '/welcome-plugin', title: 'Welcome Plugin', external: false },
+      { path: '/custom-commands', title: 'Custom Commands', external: false },
+      { path: '/reaction-roles', title: 'Reaction Roles', external: false },
+      { path: '/moderator', title: 'Moderator', external: false },
+    ],
+  },
+  {
+    title: 'Utilities',
+    subitems: [
+      { path: '/embeds', title: 'Embeds', external: false },
+      { path: '/search-anything', title: 'Search Anything', external: false },
+      { path: '/record', title: 'Record', external: false },
+    ],
+  },
+]
+
 const { user, checkCallback, checkSessionStorage, logout } = useAuth()
 const router = useRouter()
 
@@ -180,6 +287,12 @@ watch(colorMode, (newMode) => {
 const avatarFallbackText = computed(() => {
 	return user?.value?.username.slice(0, 2)
 })
+
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
 
 const handleNavigation = (path) => {
 	if (path.startsWith('http')) {
