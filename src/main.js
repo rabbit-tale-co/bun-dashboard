bf54ed098bd3d3cac42577fba13823fa94ee999e
poker-cats-co <hasiradoo@starowl.dev>
2024-07-02 13:42:35 +0200

    added components

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import './index.css'
 import router from './router'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+import Servers from '@/components/sections/Servers.vue'
+import Hero from '@/components/sections/Hero.vue'
+import ServerCard from '@/components/sections/ServerCrad.vue'
+import Features from '@/components/sections/Features.vue'
+import Statistics from '@/components/sections/Statistics.vue'
+import LeaderBoard from '@/components/sections/Leaderboard.vue'
+
+
+createApp(App)
+.component('Servers', Servers)
+.component('Hero', Hero)
+.component('ServerCard', ServerCard)
+.component('Features', Features)
+.component('Statistics', Statistics)
+.component('Leaderboard', LeaderBoard)
+.use(router)
+.mount('#app')
