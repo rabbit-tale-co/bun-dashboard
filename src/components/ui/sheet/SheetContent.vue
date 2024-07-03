2ec3f809d484caf01aeb92297f94a97a276cd3ae
poker-cats-co <hasiradoo@starowl.dev>
2024-07-03 18:57:37 +0200

    removed prerender close button

--- src/components/ui/sheet/SheetContent.vue
+++ src/components/ui/sheet/SheetContent.vue
@@ -54,11 +54,11 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     >
       <slot />
 
-      <DialogClose
+      <!-- <DialogClose
         class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
       >
         <X class="w-4 h-4 text-muted-foreground" />
-      </DialogClose>
+      </DialogClose> -->
     </DialogContent>
   </DialogPortal>
 </template>
