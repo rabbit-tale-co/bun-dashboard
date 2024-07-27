2b33de0482ccaf8b37a68bd975cb88d481a35c61
poker-cats-co <hasiradoo@starowl.dev>
2024-07-27 13:41:41 +0200

    added animated numbers

--- src/components/sections/LeaderboardCard.vue
+++ src/components/sections/LeaderboardCard.vue
@@ -1,31 +1,40 @@
 <template>
-   <div class="bg-card-hover rounded-lg px-6 py-5 flex items-center justify-start h-24 overflow-x-hidden">
-     <p :class="getRankClass((currentPage - 1) * usersPerPage + index)">
-       #{{ (currentPage - 1) * usersPerPage + index + 1 }}
-     </p>
-     <img :src="user.avatarUrl" class="size-12 rounded-full ml-4" :alt="user.username">
-     <div class="ml-4 text-left text-base">
-       <div class="flex items-start justify-start text-white">
-         <p class="font-bold line-clamp-1">{{ user.globalName }}</p>
-       </div>
-       <div class="flex items-center justify-start text-muted-foreground">
-         <p class="text-base">{{ commaFormatter.format(user.xp) }} XP</p>
-       </div>
-     </div>
-     <SolidCrown v-if="((currentPage - 1) * usersPerPage + index) === 0" class="ml-auto text-premium" />
-   </div>
- </template>
- 
- <script setup>
-import { computed } from 'vue'
+  <div class="bg-card-hover rounded-lg px-6 py-5 flex items-center justify-start h-24 overflow-x-hidden">
+    <p :class="getRankClass((currentPage - 1) * usersPerPage + index)">
+      #{{ (currentPage - 1) * usersPerPage + index + 1 }}
+    </p>
+    <img :src="user.avatarUrl" class="size-12 rounded-full ml-4" :alt="user.username">
+    <div class="ml-4 text-left text-base">
+      <div class="flex items-start justify-start text-white">
+        <p class="font-bold line-clamp-1">{{ user.globalName }}</p>
+      </div>
+      <div class="flex items-center justify-start text-muted-foreground">
+        <p class="text-base tabular-nums">{{ formattedXP }} XP</p>
+      </div>
+    </div>
+    <SolidCrown v-if="((currentPage - 1) * usersPerPage + index) === 0" class="ml-auto text-premium" />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue'
 import { SolidCrown } from '@/components/ui/icons'
 import { commaFormatter } from '@/utils/formatter'
+import { animateNumber } from '@/utils/animateNumber'
 
 const props = defineProps({
 	user: Object,
 	index: Number,
 	currentPage: Number,
 	usersPerPage: Number,
+})
+
+const formattedXP = ref('0')
+
+onMounted(() => {
+	animateNumber(0, props.user.xp, 3000, (value) => {
+		formattedXP.value = commaFormatter.format(value)
+	})
 })
 
 const getRankClass = (rank) => {
@@ -35,4 +44,3 @@ const getRankClass = (rank) => {
 	return 'text-white/30'
 }
 </script>
- 
